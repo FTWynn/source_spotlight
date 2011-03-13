93ffab7569c12720ade307be14db1605e7965467
David Wynn <Remixer96@gmail.com>
2011-03-12 19:00:50 -0800

    Added Pages controller and edited application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SourceSpotlight::Application.routes.draw do
+
+  match "/contact", :to => "pages#contact"
+  match "/about", :to => "pages#about"
   get "news_orgs/new"
-
   get "quotes/new"
-
   get "people/new"
-
   get "stories/new"
+
+  root :to => "pages#home"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,7 +58,7 @@ SourceSpotlight::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+
 
   # See how all your routes lay out with "rake routes"
 
